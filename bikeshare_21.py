b5bf5ebbf07af7b619a799597ce84ab620733a38
Julia-Franz <julia.franz@audi.de>
2024-12-27 12:27:34 +0100

    Refact.py - usage of auxiliary functions for user input

--- bikeshare_21.py
+++ bikeshare_21.py
@@ -5,7 +5,7 @@ import numpy as np
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
-
+# Proposal 1: usage of auxiliary functions for user input 
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
